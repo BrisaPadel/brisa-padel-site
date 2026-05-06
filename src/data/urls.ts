cf6d3f07fd0d828e14d9cf0713e840e77e8a3367
navneet <navneet@logicfirst.io>
2026-05-06 21:56:23 +0530

    changes

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -2,7 +2,7 @@ export const SITE_BASE_URL = 'https://website.brisapadel.com';
 export const WEB_BASE_URL = 'https://stage.brisapadel.com';
 
 export function siteUrl(path: string) {
-  return `${SITE_BASE_URL}${path}`;
+  return path;
 }
 
 export function webUrl(path: string) {
